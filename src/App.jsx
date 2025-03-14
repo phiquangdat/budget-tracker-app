489e96ebb1250cf4237b0b8a52c1ca1ed64cd381
phiquangdat <datphi020603@gmail.com>
2025-03-14 13:27:22 +0200

    change => {} to => () in TransactionList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react';
-import './App.css'
-import './component/TransactionList/TransactionList.jsx';
-import './component/TransactionForm.jsx';
-import TransactionList from './component/TransactionList/TransactionList.jsx';
-import TransactionForm from './component/TransactionForm.jsx';
+import { useState } from "react";
+import "./App.css";
+import TransactionList from "./component/TransactionList/TransactionList";
+import TransactionForm from "./component/TransactionForm";
 function App() {
   const [list, setList] = useState([]);
-  
+
   return (
-      <div className="container">
-        <h1>Budget tracker</h1>
-        <div className="balance-box">
-            <h3>Saldo</h3>
-            <div id="balance"></div>
-        </div>
-        <TransactionForm list={list} setList={setList}/>
-    <div>
+    <div className="container">
+      <h1>Budget tracker</h1>
+      <div className="balance-box">
+        <h3>Saldo</h3>
+        <div id="balance"></div>
+      </div>
+      <TransactionForm list={list} setList={setList} />
+      <TransactionList list={list} />
+      {/* <div>
         <canvas id="myChart"></canvas>
-    </div>
+      </div> */}
     </div>
   );
 }
 
-export default App
+export default App;
--- src/component/TransactionList/TransactionList.jsx
+++ src/component/TransactionList/TransactionList.jsx
@@ -1,15 +1,15 @@
-import './TransactionList.css'
-export default function TransactionList({list}){
-    return (
-        <ul id="transaction-list">
-            <h3>Transactions</h3>
-            {list.map(item => {
-                <li>
-                    <p className="description">${item.description}</p>
-                    <p className="sum">${item.sum}</p>
-                    <button class="delete">Poista</button>
-                </li>
-            })}
-        </ul>
-    );
+import "./TransactionList.css";
+export default function TransactionList({ list }) {
+  return (
+    <div>
+      <h3>Transactions</h3>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item.description}: {item.sum}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
